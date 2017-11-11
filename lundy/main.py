26b2b5f9d42c4fedfd0a05498e9702406b71c69b
Rafal <lizonr@gmail.com>
2017-11-11 13:33:46 +0100

    Create package cleanup

--- lundy/main.py
+++ lundy/main.py
@@ -28,7 +28,7 @@ class Lundy:
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 project_dir = os.path.join(dir_path, 'test', 'sample_project_dir')
-project = LundyProject("Lundy", project_dir)
-project.scan()
+project = LundyProject("Lundy")
+project.scan(project_dir)
 print("")
 print(project.__dict__)
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Lundy',
+    version='0.1dev',
+    packages=['lundy',],
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    long_description="Test",
+)
