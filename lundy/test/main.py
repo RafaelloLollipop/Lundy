a13c8d7ac4290417c03ecdb687aa4d7d9c320a07
Rafal <lizonr@gmail.com>
2017-11-13 14:44:31 +0100

    Fix imports

--- lundy/test/main.py
+++ lundy/test/main.py
@@ -1,6 +1,6 @@
 import os
 
-from app.datasets import LundyProject
+from lundy.datasets import LundyProject
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 project_dir = os.path.join(dir_path, 'sample_project_dir')
@@ -11,7 +11,7 @@ print(project.hash)
 
 import requests
 url = 'http://127.0.0.1:8000/api/lundy_objects/'
-data = {'data': project.to_json(), 'hash': project.hash, 'id':2}
+data = {'data': project.to_json(), 'hash': project.hash, 'id':1}
 requests.post(url, json=data)
 
 
