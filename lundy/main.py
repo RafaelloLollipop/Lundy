39ebd28ae6bedb149f555020826f0fb7163965a9
Rafal <lizonr@gmail.com>
2017-11-15 13:04:01 +0100

    Collector can collect from class, not only from single method

--- lundy/main.py
+++ lundy/main.py
@@ -1,4 +1,6 @@
 import datetime
+import inspect
+
 from boltons.funcutils import wraps
 
 from datasets import Method, LundyMethod
@@ -7,27 +9,37 @@ from datasets import Method, LundyMethod
 class Lundy:
     @staticmethod
     def collector(*spec_args, **spec_kwargs):
-        def collector_decorator(func):
-            @wraps(func)
-            def func_wrapper(*args, **kwargs):
-                loc = locals()
-                start_time = datetime.datetime.now()
-                result = func(*args, **kwargs)
-                duration = (datetime.datetime.now() - start_time).total_seconds()
-                lundy_method = LundyMethod(loc['func'].__name__)
-                lundy_method.scan(func)
-                method_hash = lundy_method.hash
-                m = Method(name=loc['func'].__name__,
-                           args=args,
-                           kwargs=kwargs,
-                           result=result,
-                           hash=method_hash,
-                           start_time=start_time,
-                           duration=duration
-                           )
-                m.save()
-                return result
-
-            return func_wrapper
+        def collector_decorator(obj):
+            if inspect.isfunction(obj):
+                return method_wrapper(obj)
+            elif inspect.isclass(obj):
+                for method in obj.__dict__:
+                    method_object = getattr(obj, method)
+                    if inspect.ismethod(method_object):
+                        setattr(obj, method, method_wrapper(method_object))
+                return obj
 
         return collector_decorator
+
+
+def method_wrapper(obj):
+    @wraps(obj)
+    def func_wrapper(*args, **kwargs):
+        start_time = datetime.datetime.now()
+        result = obj(*args, **kwargs)
+        duration = (datetime.datetime.now() - start_time).total_seconds()
+        lundy_method = LundyMethod(obj.__name__)
+        lundy_method.scan(obj)
+        method_hash = lundy_method.hash
+        m = Method(name=obj.__name__,
+                   args=args,
+                   kwargs=kwargs,
+                   result=result,
+                   hash=method_hash,
+                   start_time=start_time,
+                   duration=duration
+                   )
+        m.save()
+        return result
+
+    return func_wrapper
